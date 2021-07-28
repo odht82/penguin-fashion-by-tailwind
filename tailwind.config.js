2ec70af47f89e3e8e0d9e00d845802f36a346ffa
Md Obaydul Hoque <tahiduli723@gmail.com>
2021-07-29 03:32:15 +0600

    header section completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,23 @@ module.exports = {
         neue: "'Bebas Neue', cursive"
       },
       colors: {
-        primarybg: '#FFFBF0'
+        primarybg: '#FFFBF0',
+        primary: {
+          100: '#A4BC46',
+          200: '#85A019'
+        },
+        lime: {
+          50: '#F7FEE7',
+          100: '#ECFCCB',
+          200: '#D9F99D',
+          300: '#BEF264',
+          400: '#A3E635',
+          500: '#84CC16',
+          600: '#65A30D',
+          700: '#4D7C0F',
+          800: '#3F6212',
+          900: '#365314'
+        }
       }
     }
   },
